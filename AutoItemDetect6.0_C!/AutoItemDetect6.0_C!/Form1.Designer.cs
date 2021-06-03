0c033dbab3ce7ae9152dce7ea060ab6a95096dd6
wzd <wzdwxjwxw@sjtu.edu.cn>
2021-06-03 12:05:37 +0800

    Logon

--- AutoItemDetect6.0_C!/AutoItemDetect6.0_C!/Form1.Designer.cs
+++ AutoItemDetect6.0_C!/AutoItemDetect6.0_C!/Form1.Designer.cs
@@ -38,10 +38,27 @@ namespace AutoItemDetect6._0_C_
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -52,8 +69,11 @@ namespace AutoItemDetect6._0_C_
             // 
             // listView1
             // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(738, 289);
+            this.listView1.Location = new System.Drawing.Point(738, 258);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(151, 121);
             this.listView1.TabIndex = 5;
@@ -68,6 +88,7 @@ namespace AutoItemDetect6._0_C_
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -76,41 +97,132 @@ namespace AutoItemDetect6._0_C_
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.menuStrip1);
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(712, 489);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Everything";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 456);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(3, 3);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(706, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(712, 489);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Logon";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(706, 456);
+            this.dataGridView2.TabIndex = 2;
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
             this.tabPage3.Location = new System.Drawing.Point(4, 29);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Size = new System.Drawing.Size(712, 489);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.Text = "Services";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 29;
+            this.dataGridView3.Size = new System.Drawing.Size(706, 456);
+            this.dataGridView3.TabIndex = 2;
             // 
             // tabPage4
             // 
+            this.tabPage4.Controls.Add(this.dataGridView4);
             this.tabPage4.Location = new System.Drawing.Point(4, 29);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Size = new System.Drawing.Size(712, 489);
             this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.Text = "Drivers";
             this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.RowTemplate.Height = 29;
+            this.dataGridView4.Size = new System.Drawing.Size(706, 456);
+            this.dataGridView4.TabIndex = 2;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.dataGridView5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 29);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(712, 489);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Scheduled Tasks";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.RowHeadersWidth = 51;
+            this.dataGridView5.RowTemplate.Height = 29;
+            this.dataGridView5.Size = new System.Drawing.Size(706, 456);
+            this.dataGridView5.TabIndex = 2;
+            this.dataGridView5.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView5_CellContentClick);
             // 
             // Form1
             // 
@@ -119,10 +231,22 @@ namespace AutoItemDetect6._0_C_
             this.ClientSize = new System.Drawing.Size(901, 565);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.listView1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -138,6 +262,13 @@ namespace AutoItemDetect6._0_C_
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.DataGridView dataGridView5;
     }
 }
 
--- AutoItemDetect6.0_C!/AutoItemDetect6.0_C!/Form1.cs
+++ AutoItemDetect6.0_C!/AutoItemDetect6.0_C!/Form1.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 
 namespace AutoItemDetect6._0_C_
 {
@@ -19,16 +19,101 @@ namespace AutoItemDetect6._0_C_
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        // 给各个表格加上列名
+        private void Initial_columns(object sender, DataGridView tmp)
         {
+            DataGridViewTextBoxColumn autorun_entry = new DataGridViewTextBoxColumn();
 
+            DataGridViewTextBoxColumn description = new DataGridViewTextBoxColumn();
+
+            DataGridViewTextBoxColumn publisher = new DataGridViewTextBoxColumn();
+
+            DataGridViewTextBoxColumn image_path = new DataGridViewTextBoxColumn();
+
+            DataGridViewTextBoxColumn timestamp = new DataGridViewTextBoxColumn();
+
+            DataGridViewImageColumn virus_total = new DataGridViewImageColumn();
+
+            autorun_entry.HeaderText = "Autorun Entry";
+            description.HeaderText = "Description";
+            publisher.HeaderText = "Publisher";
+            image_path.HeaderText = "Image Path";
+            timestamp.HeaderText = "Timestamp";
+            virus_total.HeaderText = "Virus Total";
+
+            tmp.Columns.Add(autorun_entry);
+            tmp.Columns.Add(description);
+            tmp.Columns.Add(publisher);
+            tmp.Columns.Add(image_path);
+            tmp.Columns.Add(timestamp);
+            tmp.Columns.Add(virus_total);
+
+            return;
         }
 
-        // 获取第一个路径的所有信息
-        private void get_currentuser_autorun()
+        // 处理注册表中获得的路径，让它尽量合理
+        private string Process_illegal_path(string target_str)
         {
+            string res = null;
+
+            int n = target_str.Length;
+            if (n == 0) return res;
+
+            if(target_str[0] == '"')
+            {
+                for(int i=1; i<n; i++)
+                {
+                    if(target_str[i] == '"')
+                    {
+                        res = target_str.Substring(1, i - 1);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for(int i=1; i<n; i++)
+                {
+                    if(target_str[i] == '-' && target_str[i-1] == ' ')
+                    {
+                        res = target_str.Substring(0, i - 1);
+                        break;
+                    }
+                    if(i == (n-1))
+                    {
+                        res = target_str;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        // 根据文件路径，获取对应的信息
+        private void Get_file_information(string path)
+        {
+            // File提供了很多静态方法，直接调用即可
+            // FileInfo也是同一类型，但是方法不是静态
+            FileInfo file_process_class = new FileInfo(path);
+
+            // 根据文件路径，获取了一些基本信息
+            // 这里一直在处理各种格式信息，我真的是直接醉了
+            string name = file_process_class.Name;
+            string timestamp = file_process_class.CreationTime.ToShortDateString();
+            string ImagePath = path;
+
+
+            return;
+        }
+        private void test_of_dataGridView1(object sender, DataGridView target_dataGridView)
+        {
+            // 以下是一个比较完整的读取注册表的流程
+            // 可惜的是，并没有实现完全的前后端分离
+
+            // 想要获取详细信息，需要用shell类，这一点是需要时刻谨记的
             ListViewItem tmp = new ListViewItem(
                 new string[] { "path" });
+
             this.listView1.Items.Add(tmp);
 
             // 这里涉及到内容提取了
@@ -49,53 +134,65 @@ namespace AutoItemDetect6._0_C_
 
             res += n.ToString();
 
+            List<string> name_of_ = new List<string>();
+            List<string> val_of_ = new List<string>();
+
             for (int i = 0; i < n; i++)
             {
-                res += runData.GetValue(name_of_auto[i]);
+                string name_db = name_of_auto[i];
+                string val_db = (string)runData.GetValue(name_db);
+
+                val_db = this.Process_illegal_path(val_db);
                 res += " ";
-                res += name_of_auto[i];
+                val_of_.Add(val_db);
+                name_of_.Add(name_db);
             }
+
+            // 现在已经完成了路径的提取，逻辑较为完善了
+
+
+            /*
+            int index=this.dataGridView1.Rows.Add();
+
+            this.dataGridView1.Rows[index].Cells[0].Value = "1";
+
+            this.dataGridView1.Rows[index].Cells[1].Value = "2";
+
+            this.dataGridView1.Rows[index].Cells[2].Value = "监听";
+             */
+            // 以后要把这里的循环改成某个类，和表格一一对应是最好的
+            // 最好再加上一个简单的类别转换
+
+            foreach (string val in val_of_)
+            {
+                int index = target_dataGridView.Rows.Add();
+
+                target_dataGridView.Rows[index].Cells[0].Value = val; 
+            }
+            
+            this.listView1.Items.Add("\n" + res);
 
             this.listView1.Items.Add("\n" + res);
 
             return;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 这里初始化控件的列，是需要考虑的
+
+            this.Initial_columns(sender, this.dataGridView1);
+            this.Initial_columns(sender, this.dataGridView2);
+            this.Initial_columns(sender, this.dataGridView3);
+            this.Initial_columns(sender, this.dataGridView4);
+            this.Initial_columns(sender, this.dataGridView5);
+
+            this.test_of_dataGridView1(sender, this.dataGridView1);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ListViewItem tmp = new ListViewItem(
-                new string[] { "path" });
 
-            this.listView1.Items.Add(tmp);
-
-            // 这里涉及到内容提取了
             
-            RegistryKey currentUser = Registry.CurrentUser;
-            // 一层层向下打开
-            RegistryKey runData = currentUser.OpenSubKey(@"Software\\Microsoft\\Windows\\CurrentVersion\\Run");
-            
-            // if (runData == null) res += "failing!";
-
-            string[] name_of_auto = runData.GetValueNames();
-
-            // 以下这个键已经被关闭了
-            // currentUser.Close();
-
-            string res = "";
-            int n = name_of_auto.Length;
-
-            res += n.ToString();
-
-            for (int i=0; i<n; i++)
-            {
-                res += runData.GetValue(name_of_auto[i]);
-                res += " ";
-                res += name_of_auto[i];
-            }
-
-            this.listView1.Items.Add("\n" + res);
-
-            return;
         }
 
         // C#
@@ -115,6 +212,16 @@ namespace AutoItemDetect6._0_C_
         }
 
         private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView5_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
